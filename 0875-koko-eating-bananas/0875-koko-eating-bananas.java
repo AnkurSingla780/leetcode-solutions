bfe40229f359b7c5c0fd07a7462f538c64866caa
Ankur Singla <ankursingla1975@gmail.com>
2026-07-16 02:06:22 +0530

    Time: 9 ms (59.37%), Space: 47.9 MB (59.75%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.java
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -1,29 +1,70 @@
-class Solution {
-    public int minEatingSpeed(int[] piles, int h) {
-        int max = Integer.MIN_VALUE ;
+// class Solution {
+//     public int minEatingSpeed(int[] piles, int h) {
+//         int max = Integer.MIN_VALUE ;
         
-        for(int i: piles){
-            max = Math.max(i,max) ;
+//         for(int i: piles){
+//             max = Math.max(i,max) ;
+//         }
+
+//         int low = 1 ;
+//         int high = max ;
+//         int ans = max ;
+//         while(low<=high){
+//             long hr = 0 ;
+//             int mid = low+(high - low) /2 ;
+//             for(int j:piles){
+//                 hr +=((j+mid-1)/mid) ;
+//                 if (hr > h) break;
+//             }
+//             if(hr>h){
+//                 low = mid +1 ;
+//             }
+//             else{
+//                 high = mid -1 ;
+//                 ans = mid ;
+//             }
+//         }
+
+//         return ans ;
+//     }
+// }
+
+class Solution {
+
+    private boolean canEat(int[] piles, int speed, int h) {
+        long hours = 0;
+
+        for (int pile : piles) {
+            hours += (pile + (long)speed - 1) / speed;
+
+            if (hours > h) {
+                return false;
+            }
         }
 
-        int low = 1 ;
-        int high = max ;
-        int ans = max ;
-        while(low<=high){
-            long hr = 0 ;
-            int mid = low+(high - low) /2 ;
-            for(int j:piles){
-                hr +=((j+mid-1)/mid) ;
-            }
-            if(hr>h){
-                low = mid +1 ;
-            }
-            else{
-                high = mid -1 ;
-                ans = mid ;
+        return true;
+    }
+
+    public int minEatingSpeed(int[] piles, int h) {
+
+        int max = 0;
+        for (int pile : piles) {
+            max = Math.max(max, pile);
+        }
+
+        int low = 1;
+        int high = max;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (canEat(piles, mid, h)) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
             }
         }
 
-        return ans ;
+        return low;
     }
 }
