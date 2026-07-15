89f33b10d21bc853dca9364076daf14bb0f45c53
Ankur Singla <ankursingla1975@gmail.com>
2026-07-16 00:49:48 +0530

    Time: 1 ms (97.62%), Space: 42.7 MB (42.92%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,9 +1,9 @@
 class Solution {
     public int mySqrt(int x) {
-        if (x == 0) return 0;
+        if (x <2) return x;
         long low = 1;
         long high = x ;
-        long ans = 1 ;
+        long ans = 0 ;
         while (low<=high) {
             long mid = low+(high-low)/2 ;
 
