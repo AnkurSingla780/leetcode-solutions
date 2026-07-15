58ff8a6db0e35e1fc55cccceaef84263c1532ffe
Ankur Singla <ankursingla1975@gmail.com>
2026-07-16 00:15:06 +0530

    Time: 0 ms (100%), Space: 44.1 MB (74.03%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -6,12 +6,25 @@ class Solution {
         if(nums[0]>nums[1]){
             return 0 ;
         }
-        for(int i = 1 ;i<nums.length -1;i++){
-            if(nums[i]>nums[i+1] && nums[i]>nums[i-1]){
-                return i ;
+        if(nums.length == 2 && nums[1]>nums[0]){
+            return 1 ;
+        }
+        if(nums[nums.length-1] > nums[nums.length - 2]){return nums.length - 1 ;}
+        int  low =1 ;
+        int high = nums.length - 1 ;
+        while(low<=high){
+            int mid  = low+(high-low)/2 ;
+
+            if(nums[mid]>nums[mid+1] && nums[mid]> nums[mid-1]){
+                return mid ;
             }
+
+            if(nums[mid]>nums[mid-1]){
+                low = mid+1 ;
+            }
+            else{high = mid -1 ;}
         }
 
-        return nums.length-1 ;
+        return -1 ;
     }
 }
