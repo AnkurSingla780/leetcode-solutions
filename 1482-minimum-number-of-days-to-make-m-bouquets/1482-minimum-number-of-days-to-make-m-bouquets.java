9b3a75d2f7000e7f68a4fe14c4a3dc505cef62bb
Ankur Singla <ankursingla1975@gmail.com>
2026-07-16 03:53:49 +0530

    Time: 18 ms (50.66%), Space: 82.9 MB (12.56%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
@@ -1,59 +1,113 @@
-class Solution {
+// class Solution {
 
-    public boolean canMake(int[] bloomDay, int m, int k,int mid){
-        long flowers = 0;
-        long bouquet = 0 ;
+//     public boolean canMake(int[] bloomDay, int m, int k,int mid){
+//         long flowers = 0;
+//         long bouquet = 0 ;
 
-        for(int i = 0;i<bloomDay.length;i++){
-            if(mid>=bloomDay[i]){
-                flowers++ ;
-            }
-            else{
-                flowers = 0 ;
-            }
-            if(flowers==k){
-                bouquet++ ;
-                flowers = 0 ;
-            }
+//         for(int i = 0;i<bloomDay.length;i++){
+//             if(mid>=bloomDay[i]){
+//                 flowers++ ;
+//             }
+//             else{
+//                 flowers = 0 ;
+//             }
+//             if(flowers==k){
+//                 bouquet++ ;
+//                 flowers = 0 ;
+//             }
 
-            if(bouquet>=m){
-                return true ;
-            }
-        } 
-        return false ;
-    }
+//             if(bouquet>=m){
+//                 return true ;
+//             }
+//         } 
+//         return false ;
+//     }
 
 
-    public int minDays(int[] bloomDay, int m, int k) {
-        if(bloomDay.length<(long)m*k){
-            return -1 ;
-        }
-        if(m==0 || k==0){
-            return 0 ;
-        }
-        int min = Integer.MAX_VALUE ;
-        int max = Integer.MIN_VALUE ;
+//     public int minDays(int[] bloomDay, int m, int k) {
+//         if(bloomDay.length<(long)m*k){
+//             return -1 ;
+//         }
+//         if(m==0 || k==0){
+//             return 0 ;
+//         }
+//         int min = Integer.MAX_VALUE ;
+//         int max = Integer.MIN_VALUE ;
 
-        for(int i: bloomDay){
-            min = Math.min(min,i) ;
-            max = Math.max(max , i) ;
-        }
+//         for(int i: bloomDay){
+//             min = Math.min(min,i) ;
+//             max = Math.max(max , i) ;
+//         }
 
-        int low = min; int high = max ; int ans = -1;
+//         int low = min; int high = max ;
 
-        while(low<=high){
-            int mid = low+(high-low)/2 ;
+//         while(low<=high){
+//             int mid = low+(high-low)/2 ;
 
-            if(canMake(bloomDay,m,k,mid)){
-                high = mid -1 ;
-                ans = mid ;
-            }
-            else{
-                low = mid+1 ;
-            }
+//             if(canMake(bloomDay,m,k,mid)){
+//                 high = mid -1 ;
+//             }
+//             else{
+//                 low = mid+1 ;
+//             }
             
-        }
+//         }
         
-        return low ;
+//         return low ;
+//     }
+// }
+
+class Solution {
+    public int minDays(int[] bloomDay, int m, int k) {
+
+        if (bloomDay.length < (long) m * k) {
+            return -1;
+        }
+
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        for (int day : bloomDay) {
+            min = Math.min(min, day);
+            max = Math.max(max, day);
+        }
+
+        int low = min;
+        int high = max;
+
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            int flowers = 0;
+            int bouquets = 0;
+
+            for (int day : bloomDay) {
+
+                if (day <= mid) {
+                    flowers++;
+                } else {
+                    flowers = 0;
+                }
+
+                if (flowers == k) {
+                    bouquets++;
+                    flowers = 0;
+                }
+
+                // Early exit
+                if (bouquets >= m) {
+                    break;
+                }
+            }
+
+            if (bouquets >= m) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return low;
     }
 }
