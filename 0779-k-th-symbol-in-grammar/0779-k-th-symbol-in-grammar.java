3b58234bec185a796830cd3c60d7b04d1022cf5c
Ankur Singla <ankursingla1975@gmail.com>
2026-07-18 01:26:01 +0530

    Time: 0 ms (100%), Space: 42 MB (57.04%) - LeetHub

---
+++ 0779-k-th-symbol-in-grammar/0779-k-th-symbol-in-grammar.java
@@ -0,0 +1,21 @@
+class Solution {
+
+    public int kthGrammar(int n, int k) {
+        return solve(n, k);
+    }
+
+    public int solve(int n, int k) {
+
+        if (n == 1 && k == 1) {
+            return 0;
+        }
+
+        int mid = 1 << (n - 2);   
+
+        if (k <= mid) {
+            return solve(n - 1, k);
+        } else {
+            return 1 - solve(n - 1, k - mid);
+        }
+    }
+}
