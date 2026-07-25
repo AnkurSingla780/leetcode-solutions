0583ab8ea144165c816f74940fce735869e40949
Ankur Singla <ankursingla1975@gmail.com>
2026-07-25 14:52:09 +0530

    Time: 0 ms (100%), Space: 42 MB (73.07%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,16 +1,28 @@
 class Solution {
     public int climbStairs(int n) {
-        int[] dp = new int[n+1] ;
-        Arrays.fill(dp,-1) ;
+        // int[] dp = new int[n+1] ;
+        // Arrays.fill(dp,-1) ;
         if(n<=1){
             return n ;
         }
-        dp[1] =1 ;
-        dp[2] = 2 ;
+        // dp[1] =1 ;
+        // dp[2] = 2 ;
+        // for(int i = 3 ; i<=n ;i++){
+        //     dp[i] = dp[i-1] + dp[i-2] ;
+        // }
+        // return dp[n] ;
+
+        int prev = 2 ;
+        int prev2 = 1 ;
+
         for(int i = 3 ; i<=n ;i++){
-            dp[i] = dp[i-1] + dp[i-2] ;
+            int curr  = prev + prev2 ;
+            prev2 = prev ;
+            prev = curr ;
+            
         }
-        return dp[n] ;
+        return prev ;
+
         // return solve(n,dp) ;
         
     }
