c0165d93a9a170bce13a25439b28e5e4a61174d3
Ankur Singla <ankursingla1975@gmail.com>
2026-07-29 03:32:09 +0530

    Time: 0 ms (100%), Space: 42.9 MB (19.08%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,32 +1,59 @@
-import java.util.Arrays;
+// import java.util.Arrays;
+
+// class Solution {
+//     public int rob(int[] nums) {
+//         if (nums.length == 1) {
+//             return nums[0];
+//         }
+
+//         int[] dp = new int[nums.length];
+//         Arrays.fill(dp, -1);
+
+//         return solve(0, nums, dp);
+//     }
+
+//     public int solve(int i, int[] nums, int[] dp) {
+
+//         if (i >= nums.length) {
+//             return 0;
+//         }
+
+//         if (dp[i] != -1) {
+//             return dp[i];
+//         }
+
+//         int take = nums[i] + solve(i + 2, nums, dp);
+//         int skip = solve(i + 1, nums, dp);
+
+//         dp[i] = Math.max(take, skip);
+
+//         return dp[i];
+//     }
+// }
 
 class Solution {
     public int rob(int[] nums) {
-        if (nums.length == 1) {
+
+        int n = nums.length;
+
+        if (n == 1) {
             return nums[0];
         }
 
-        int[] dp = new int[nums.length];
-        Arrays.fill(dp, -1);
+        int prev2 = nums[0];
+        int prev = Math.max(nums[0], nums[1]);
 
-        return solve(0, nums, dp);
-    }
+        for (int i = 2; i < n; i++) {
 
-    public int solve(int i, int[] nums, int[] dp) {
+            int take = nums[i] + prev2;
+            int skip = prev;
 
-        if (i >= nums.length) {
-            return 0;
+            int curr = Math.max(take, skip);
+
+            prev2 = prev;
+            prev = curr;
         }
 
-        if (dp[i] != -1) {
-            return dp[i];
-        }
-
-        int take = nums[i] + solve(i + 2, nums, dp);
-        int skip = solve(i + 1, nums, dp);
-
-        dp[i] = Math.max(take, skip);
-
-        return dp[i];
+        return prev;
     }
 }
